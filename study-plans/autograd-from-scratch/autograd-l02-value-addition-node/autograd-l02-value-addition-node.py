812fd83fcf4f9a193fa52765101ba874300ccb0b
rohitsalla <sallarohit1011@gmail.com>
2026-09-06 13:57:30 -0400

    Solve autograd-l02-value-addition-node

---
+++ study-plans/autograd-from-scratch/autograd-l02-value-addition-node/autograd-l02-value-addition-node.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def value_addition_node(left, right, output_id):
+    """
+    Returns: an addition node that retains the two supplied leaf records as ordered parents
+    """
+    return {
+        'id':      output_id,
+        'data':    float(np.float64(left['data']) + np.float64(right['data'])),
+        'grad':    0.0,
+        'op':      '+',
+        'parents': [left, right],   # original objects, not copies
+    }
